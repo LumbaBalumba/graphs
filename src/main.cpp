443dfd5f6cea518ec4809a7b615cbec0a766934f
LumbaBalumba <vlad.lazar.ik528@yandex.ru>
2025-08-19 21:03:08 +0300

    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <print>
+#include <random>
+#include <ranges>
 
 #include "graph_processor.hpp"
 #include "node_graph.hpp"
@@ -7,6 +9,23 @@ NodeGraph<>
 build_graph()
 {
     NodeGraph graph;
+
+    for (auto _: std::ranges::views::iota(0, 200)) {
+        graph.add_node();
+    }
+
+    std::random_device rd;
+    std::mt19937 rng(rd());
+    std::uniform_int_distribution<> node_ids(0, 199);
+    std::uniform_real_distribution<> edge_lengths(1.0, 10.0);
+
+
+    for (auto _: std::ranges::views::iota(0, 1000)) {
+        int left = node_ids(rng);
+        int right = node_ids(rng);
+        double weight = edge_lengths(rng);
+        graph.add_edge({left, right}, weight);
+    }
 
     return graph;
 }
